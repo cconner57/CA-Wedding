098e86354710041b09442de9beca8e979764016a
Chris <chris.conner57@gmail.com>
2019-12-11 16:24:10 -0800

    Converted to React

--- navbar.js
+++
@@ -1,8 +0,0 @@
-window.scroll(() => {
-    if (window.scrollTop() > 1){  
-      document.querySelector('#nav-bar').setAttribute('style', 'position: sticky;');
-    }
-    else {
-        document.querySelector('#nav-bar').removeAttribute('style', 'position: sticky;');
-    }
-  });
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/Navbar/navbar';
+// import BGVideo from './components/BG-Video/bg-video';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <BGVideo /> */}
+    </div>
+  );
+}
+
+export default App;
