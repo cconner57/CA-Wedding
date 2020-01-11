ecac4a923396c312dee2721ad38f5b70b5d6d458
Chris <chris.conner57@gmail.com>
2020-01-10 18:08:47 -0800

    Reset form

--- src/components/RSVP/rsvp.js
+++ src/components/RSVP/rsvp.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import './rsvp.css';
+import axios from 'axios';
 
 function RSVP() {
+	const [ name, setName ] = useState('');
+	const [ value, setValue ] = useState(null);
+
+	const handleChange = (event) => {
+		setName(event.target.value);
+		setValue(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+	
 	return (
 		<div className="rsvp-form">
 			<h1>C &amp; A</h1>
@@ -12,9 +25,9 @@ function RSVP() {
 				<h5>Kindly Reply By 'Enter Date'</h5>
 			</div>
 			<hr />
-			<form name="RSVP" data-netlify="true">
+			<form name="RSVP" onSubmit={handleSubmit}>
 				<div className="rsvp-name">
-					<input type="text" name="Name" autoComplete="off" required />
+					<input type="text" name="Name" onChange={handleChange} autoComplete="off" required />
 					<label htmlFor="nameInput" className="label-name">
 						<span className="content-name">First &amp; Last Name</span>
 					</label>
@@ -22,8 +35,8 @@ function RSVP() {
 				<div className="rsvp-attending">
 					<label>
 						Attending:
-						<input type="radio" id="radio-1" name="Attending" value="yes" />Accept
-						<input type="radio" id="radio-1" name="Attending" value="no" />Decline
+						<input type="radio" id="radio-1" name="Attending" value="yes" onChange={handleChange} />Accept
+						<input type="radio" id="radio-1" name="Attending" value="no" onChange={handleChange} />Decline
 					</label>
 				</div>
 				<div>
