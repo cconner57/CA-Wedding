cbe5f880ee15ef5ac98b6688ff0c4ec080ac00d1
Chris <chris.conner57@gmail.com>
2020-02-17 09:18:06 -0800

    Added link directions

--- src/components/reception.js
+++ src/components/reception.js
@@ -11,6 +11,7 @@ function Location() {
 						<p>Time: TBA</p>
 						<p>6300 Country Club Drive</p>
 						<p>La Verne, Ca 91750</p>
+						<a href="https://goo.gl/maps/d967vEESQ55Q6sqQ6">Click for Directions</a>
 					</div>
 					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13209.24886026702!2d-117.7679308!3d34.1383536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe452ea9c719ae746!2sSierra%20La%20Verne%20Wedgewood%20Weddings!5e0!3m2!1sen!2sus!4v1581910801052!5m2!1sen!2sus" />
 				</div>
