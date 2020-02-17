308eee5494a51210fe85d45d506779106d79410a
Chris <chris.conner57@gmail.com>
2020-02-17 10:38:38 -0800

    Changed directions url

--- src/components/reception.js
+++ src/components/reception.js
@@ -11,7 +11,7 @@ function Location() {
 						<p>Time: TBA</p>
 						<p>6300 Country Club Drive</p>
 						<p>La Verne, Ca 91750</p>
-						<a href="https://maps.apple.com/?address=6300%20Country%20Club%20Dr,%20La%20Verne,%20CA%20%2091750,%20United%20States&auid=10642440379529879294&ll=34.139874,-117.768236&lsp=9902&q=Sierra%20La%20Verne%20by%20Wedgewood%20Weddings&_ext=ChoKBQgEEOIBCgQIBRADCgUIBhDfAQoECAoQABImKXEdPjVUEUFAMReeR7GDcV3AOe/yY5F6EkFAQY3vNt7RcF3AUAM%3D">Click for Directions</a>
+						<a href="https://maps.apple.com/?address=6300%20Country%20Club%20Dr,%20La%20Verne,%20CA%20%2091750,%20United%20States">Click for Directions</a>
 					</div>
 					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13209.24886026702!2d-117.7679308!3d34.1383536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe452ea9c719ae746!2sSierra%20La%20Verne%20Wedgewood%20Weddings!5e0!3m2!1sen!2sus!4v1581910801052!5m2!1sen!2sus" />
 				</div>
